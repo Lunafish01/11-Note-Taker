817bfc807760f63d665e1ea2a5868233cc274ba8
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-06 00:37:29 -0600

    added html routes and PORT connection

--- server.js
+++ server.js
@@ -2,3 +2,29 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
+const PORT = 3001;
+
+//write function to create new notes
+
+
+//set up DELETE route
+//write function to delete notes
+
+
+//set up HTML routes
+//GET notes should return the notes.html file.
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+//GET * should return the index.html file.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Listen for connections
+app.listen(PORT, () =>
+  console.info(`Server now listening at http://localhost:${PORT}`)
+);
+
+
