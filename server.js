3c7c4ff518a9399fa4a7378ca480716a214f9af0
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-11-07 22:41:11 -0600

    pushing to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //initialize an array to store notes
 const allNotes = [];
